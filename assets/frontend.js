4e6ebd06eb44be10f24ce97fbbe7a68c6b0bd880
liushichao <517662851@qq.com>
2023-11-26 18:54:54 +0800

    finish

--- assets/frontend.js
+++ assets/frontend.js
@@ -1,49 +1,21 @@
+
+import RegionsPlugin from './regions.esm.js'
 class ClipVideo {
     constructor() {
+        this.wavesurfer = null
+        this.wsRegions = null
         this.ffmpeg = null
         this.originFile = null
-        this.handleFile = null
-        this.vueInstance = null
-        this.currentSliderValue = [0, 0]
         this.init()
     }
     init() {
         console.log('init')
+        this.initWavesurfe();
         this.initFfmpeg()
         this.bindSelectOriginFile()
-        this.bindOriginVideoLoad()
-        this.bindClipBtn()
-        this.initVueSlider()
+        this.bindCutBtn()
     }
-    initVueSlider(maxSliderValue = 100) {
-        console.log(`maxSliderValue ${maxSliderValue}`)
-        if (!this.vueInstance) {
-            const _this = this
-            const Main = {
-                data() {
-                    return {
-                        value: [0, 0],
-                        maxSliderValue: maxSliderValue
-                    }
-                },
-                watch: {
-                    value() {
-                        _this.currentSliderValue = this.value
-                    }
-                },
-                methods: {
-                    formatTooltip(val) {
-                        return _this.transformSecondToVideoFormat(val);
-                    }
-                }
-            }
-            const Ctor = Vue.extend(Main)
-            this.vueInstance = new Ctor().$mount('#app')
-        } else {
-            this.vueInstance.maxSliderValue = maxSliderValue
-            this.vueInstance.value = [0, 0]
-        }
-    }
+    
     transformSecondToVideoFormat(value = 0) {
         const totalSecond = Number(value)
         let hours = Math.floor(totalSecond / (60 * 60))
@@ -76,47 +48,70 @@ class ClipVideo {
             corePath: './assets/ffmpeg-core.js',
         });
     }
+    initWavesurfe() {
+        this.wavesurfer = WaveSurfer.create({
+            interact: false,
+            container: '#waveform'
+        });
+        
+        this.wsRegions = this.wavesurfer.registerPlugin(RegionsPlugin.create());
+        this.wsRegions.on('region-clicked', (region, e) => {
+            e.stopPropagation() // prevent triggering a click on the waveform
+            activeRegion = region
+            region.play()
+            region.setOptions({ color: randomColor() })
+        })
+
+        this.wsRegions.enableDragSelection({
+            color: 'rgba(255, 0, 0, 0.1)',
+        })
+    }
+    bindCutBtn() {
+        btnCut.addEventListener('click',  async () => {
+            const regions = this.wsRegions.getRegions();
+            console.log(regions);
+            for (var i = 0; i < regions.length; i ++) {
+                await this.clipAudio(this.originFile, regions[i].start, regions[i].end, i)
+            }
+        });
+    }
     bindSelectOriginFile() {
         $('#select_origin_file').on('change', (e) => {
             const file = e.target.files[0]
             this.originFile = file
             const url = window.webkitURL.createObjectURL(file)
-            $('#origin-video').attr('src', url)
-
+            this.wavesurfer.load(url);
         })
     }
-    bindOriginVideoLoad() {
-        $('#origin-video').on('loadedmetadata', (e) => {
-            const duration = Math.floor(e.target.duration)
-            this.initVueSlider(duration)
-        })
-    }
-    bindClipBtn() {
-        $('#start_clip').on('click', () => {
-            console.log('start clip')
-            this.clipFile(this.originFile)
-        })
-    }
-    async clipFile(file) {
-        const { ffmpeg, currentSliderValue } = this
+    async clipAudio(file, start, end, idx) {
+        const { ffmpeg } = this
         const { fetchFile } = FFmpeg;
         const { name } = file;
-        const startTime = this.transformSecondToVideoFormat(currentSliderValue[0])
-        const endTime = this.transformSecondToVideoFormat(currentSliderValue[1])
+        const startTime = this.transformSecondToVideoFormat(start)
+        const endTime = this.transformSecondToVideoFormat(end)
         console.log('clipRange', startTime, endTime)
         if (!ffmpeg.isLoaded()) {
             await ffmpeg.load();
         }
         ffmpeg.FS('writeFile', name, await fetchFile(file));
         console.log('writeFile name: ' + name)
-        await ffmpeg.run('-i', name, '-ss', startTime, '-to', endTime, 'output.mp4');
-        const data = ffmpeg.FS('readFile', 'output.mp4');
-        const tempURL = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+        await ffmpeg.run('-i', name, '-ss', startTime, '-to', endTime, '-c:a', 'copy' , idx + '.wav');
+        const data = ffmpeg.FS('readFile', idx + '.wav');
+        const tempURL = URL.createObjectURL(new Blob([data.buffer], { type: 'audio/wav' }));
+        var elementA = document.createElement('a');
+        //文件的名称为时间戳加文件名后缀
+        elementA.download = + idx + ".wav";
+        elementA.style.display = 'none';
         
-        console.log('tempURL: ' + tempURL)
-        $('#handle-video').attr('src', tempURL)
+        //生成一个指向blob的URL地址，并赋值给a标签的href属性
+        elementA.href = tempURL;
+        document.body.appendChild(elementA);
+        elementA.click();
+        document.body.removeChild(elementA);
+        console.log(idx + '.wav')
     }
 }
+
 $(document).ready(function () {
     const instance = new ClipVideo()
 });
